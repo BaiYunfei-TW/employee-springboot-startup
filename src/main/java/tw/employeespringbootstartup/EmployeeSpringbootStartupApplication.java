df730bb1eafb97bb6abf8a398c1fb780e2b84f5a
BaiYunfei-TW <baiyunfei_tw@foxmail.com>
2018-05-02 11:21:02 +0800

    init project

---
+++ src/main/java/tw/employeespringbootstartup/EmployeeSpringbootStartupApplication.java
@@ -0,0 +1,13 @@
+package tw.employeespringbootstartup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+EmployeeSpringbootStartupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeSpringbootStartupApplication.class, args);
+	}
+}
