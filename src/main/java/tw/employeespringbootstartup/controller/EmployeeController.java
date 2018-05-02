3fa54328c9a04635f1439435149ec8cd14eb8ffa
BaiYunfei-TW <baiyunfei_tw@foxmail.com>
2018-05-02 12:14:40 +0800

    实现增删改差的接口

--- src/main/java/tw/employeespringbootstartup/controller/EmployeeController.java
+++ src/main/java/tw/employeespringbootstartup/controller/EmployeeController.java
@@ -1,18 +1,38 @@
 package tw.employeespringbootstartup.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import tw.employeespringbootstartup.entity.Employee;
+import tw.employeespringbootstartup.repository.EmployeeRepository;
+import tw.employeespringbootstartup.vo.ResultJson;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/employee")
+@RequestMapping("/employees")
 public class EmployeeController {
 
-    @RequestMapping("")
-    public Employee index(){
-        Employee employee = new Employee();
+    @Autowired
+    EmployeeRepository employeeRepository;
 
-        return employee;
+    @RequestMapping("")
+    public List<Employee> index(){
+        return employeeRepository.queryList();
+    }
+
+    @RequestMapping("/saveOrUpdate")
+    public ResultJson save(@RequestBody Employee[] employee) {
+        employeeRepository.saveOrUpdate(employee);
+        return ResultJson.success();
+    }
+
+    @RequestMapping("/delete")
+    public ResultJson delete(@RequestBody Integer[] ids) {
+        employeeRepository.deleteById(ids);
+        return ResultJson.success();
     }
 
 }
